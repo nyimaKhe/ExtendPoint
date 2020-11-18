06f0d5a0f7770f0ea1c6557497d8b5b6a9e05c32
Nyima Khedup <nykhedup@gmail.com>
2020-11-18 15:39:56 -0500

    midified cylinder

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,4 +1,26 @@
 
-public class Cylinder {
+public class Cylinder extends Circle{
+	double height;
+	Cylinder(){
+		
+	}
+	Cylinder(double h){
+		height = h;
+	}
 
+	Cylinder(double h, double r, int a, int b){
+		super(a,b,r);
+		height = h;
+		
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double h) {
+		height = h;
+	}
+	public double getVolume() {
+		return getArea() *height;
+	}
+	
 }
